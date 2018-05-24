1d32b9642659ede31623b4fc06783f61e245053f
Aarth Thakore <aarth.thakore@gmail.com>
2018-05-24 11:40:50 -0500

    Fleshed out my examples a little.

--- src/com/cooksys/ftd/assignments/objects/Main.java
+++ src/com/cooksys/ftd/assignments/objects/Main.java
@@ -14,20 +14,24 @@ public class Main {
 	}
 	
 	public static void ArrayListExample() { //...Do I really have to come up with ANOTHER implementation of an ArrayList at this point?
-		List<String> listExample = new ArrayList<String>(); 
-		listExample.add("A");
-		listExample.add("B");
-		listExample.add("D");
-		listExample.add(null); //ArrayList will accept a null value as a value.
+		List<Character> listExample = new ArrayList<Character>(); 
+		listExample.add('B'); //Duplicate
+		listExample.add('A');
+		listExample.add('D');
+		listExample.add('B'); //Duplicate
+//		listExample.add(null); //ArrayList will accept a null value as a value.
 		for(int i = 0; i < listExample.size(); i++) { //While a LinkedList iterates through the use of pointers, ArrayList iterates using sequential indices for its entries.
 			System.out.println("Index " + i + " contains " + listExample.get(i)); //This gives the user the ability to create functions that use counters.
 		}
-		
+		System.out.println("---");
+		Collections.sort(listExample);
+		for(int i = 0; i < listExample.size(); i++) { //While a LinkedList iterates through the use of pointers, ArrayList iterates using sequential indices for its entries.
+			System.out.println("Index " + i + " contains " + listExample.get(i)); //This gives the user the ability to create functions that use counters.
+		}
 	}
 	
 	public static void HashSetExample() { //Bingo
-		Set<String> setExample = new HashSet<String>(); 
-		int iter = 0;
+		Set<String> setExample = new HashSet<String>();
 		setExample.add("B-2"); 
 		setExample.add("B-13"); 
 		setExample.add("B-45"); 
@@ -38,12 +42,13 @@ public class Main {
 		setExample.add("G-13");
 		setExample.add("G-11");
 		setExample.add("G-9");
-		setExample.addAll(null); //HashSet will accept a null value, as well.
-//		setExample.get(4); //HashSet does not track insertion order, therefore, it is not possible to call an entry by index.
+//		setExample.addAll(null); //HashSet will accept a null value, as well.
+		setExample.get(4); //HashSet does not track insertion order, therefore, it is not possible to call an entry by index.
 		Iterator<String> iterator = setExample.iterator(); //Retrieving from a HashSet requires the use of an iterator essentially reach into the set and pull out a random value.
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
 		}
+//		Collections.sort(setExample);
 	}
 	
 	public static void HashMapExample() { //VendingMachine
@@ -53,8 +58,14 @@ public class Main {
 		mapExample.put("A3", "Sprite");
 		mapExample.put("A4", "Mello Yello");
 		mapExample.put("A5", "Powerade");
-//		mapExample.put("A5", null); //HashMap will NOT accept a null value.
+//		mapExample.put("A5", "Powerade");
+//		mapExample.put("A5", "Hi-C");
+		mapExample.put("A6", null); //HashMap will accept a null value
+		mapExample.put(null, "Beverly"); //And accept a null key.
 		mapExample.get(5); //Like the HashSet, MashMap does not track insertion order.
-		System.out.println(mapExample.get("A4"));
+		System.out.println(mapExample.get("A5"));
+		System.out.println(mapExample.get("A6"));
+		System.out.println(mapExample.get(null));
+		Collections.sort(mapExample);
 	}
 }
